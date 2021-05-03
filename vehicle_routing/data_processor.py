4874b05368f0b22ed84e40f3198eb4d7c7cd8ed4
alexey-x <avoevodkin@gmail.com>
2021-05-03 22:28:04 +0300

    Write data_processor skeleton
    Work on #17

--- vehicle_routing/data_processor.py
+++ vehicle_routing/data_processor.py
@@ -1,8 +1,41 @@
-import pandas as pd
 
 
-def main():
-    print("ok")
+class DataProcessor:
+    def __init__(self):
+        pass
+
+    def get_max_period(self):
+        pass
+    def get_depot(self):
+        pass
+    def get_final_trip(self):
+        pass
+    def get_trips(self):
+        pass
+    def get_cities(self):
+        pass
+    def get_all_cities(self):
+        pass
+    def get_routes(self):
+        pass
+    def get_route_distance(self):
+        pass
+    def get_route_time(self):
+        pass
+    def get_cars(self):
+        pass
+    def get_car_capacity(self):
+        pass
+    def get_car_price(self):
+        pass
+    def get_order_deliver_from(self):
+        pass
+    def get_order_deliver_to(self):
+        pass
+    def get_order_demand(self):
+        pass
+    def get_order_price(self):
+        pass
 
 if __name__ == "__main__":
     main()
