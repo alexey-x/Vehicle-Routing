62420b8c4ac23abd3b88e35630513777cca4c1ff
alexey-x <avoevodkin@gmail.com>
2021-05-04 09:38:50 +0300

    Format data_reader with black  #17

--- vehicle_routing/data_reader.py
+++ vehicle_routing/data_reader.py
@@ -1,27 +1,30 @@
 import pandas as pd
 
+
 class DataReader:
-    def __init__(self, in_file:str)->None:
+    def __init__(self, in_file: str) -> None:
         self.in_file = in_file
-    def read_parameters(self)->pd.DataFrame:
+
+    def read_parameters(self) -> pd.DataFrame:
         sheet_name = "Parameters"
         return pd.read_excel(self.in_file, sheet_name=sheet_name).set_index("ParamName")
 
-    def read_cities(self)->pd.DataFrame:
+    def read_cities(self) -> pd.DataFrame:
         sheet_name = "Cities"
         return pd.read_excel(self.in_file, sheet_name=sheet_name)
 
-    def read_vehicles(self)->pd.DataFrame:
+    def read_vehicles(self) -> pd.DataFrame:
         sheet_name = "Vehicles"
         return pd.read_excel(self.in_file, sheet_name=sheet_name)
 
-    def read_orders(self)->pd.DataFrame:
+    def read_orders(self) -> pd.DataFrame:
         sheet_name = "Orders"
         return pd.read_excel(self.in_file, sheet_name=sheet_name)
 
-    def read_routes(self)->pd.DataFrame:
+    def read_routes(self) -> pd.DataFrame:
         sheet_name = "Routes"
         return pd.read_excel(self.in_file, sheet_name=sheet_name)
+
 
 if __name__ == "__main__":
     import sys
