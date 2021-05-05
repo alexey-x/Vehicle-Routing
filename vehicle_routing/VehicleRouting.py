497f23c5653c03c67a79b75e7aa721a033b7c9bc
alexey-x <avoevodkin@gmail.com>
2021-05-05 10:50:28 +0300

    Add data_writer
    Work on #17

--- vehicle_routing/VehicleRouting.py
+++ vehicle_routing/VehicleRouting.py
@@ -1,4 +1,5 @@
 import sys
+from os import path
 
 from data_reader import DataReader
 from data_processor import DataProcessor
@@ -6,19 +7,24 @@ from data_writer import DataWriter
 from linprog_maker import LinProgMaker
 
 
-def run(in_file: str) -> None:
+def run(in_file: str, out_file: str) -> None:
     reader = DataReader(in_file)
     processor = DataProcessor(reader)
     task = LinProgMaker(processor)
     task.solve()
+    DataWriter(task, out_file).save()
 
 
 def main():
     in_file = sys.argv[1]
-    print(in_file)
+    in_path, in_file_name = path.split(in_file)
+    out_file = path.join(in_path, "output_" + in_file_name)
+    print(f"Input file -> {in_file}")
+    print(f"Output file -> {out_file}")
+
     try:
         in_file = sys.argv[1]
-        run(in_file)
+        run(in_file, out_file)
     except IndexError:
         print("Nothing to read")
 
